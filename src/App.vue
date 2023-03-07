abbb092b94edee04949aeac419f3fd1a5d13737f
hoon choi <photiary@gmail.com>
2023-03-07 23:04:39 +0900

    개발 가이드라인 페이지 추가

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/guideline">Guideline</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/GuidelineView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { formatSimpleDate } from '@/plugins/formatters'
+</script>
+
+<template>
+  <div>
+    <h1>Guideline page</h1>
+    <div>날짜: {{ formatSimpleDate(new Date()) }}</div>
+  </div>
+</template>
+
+<style scoped></style>
